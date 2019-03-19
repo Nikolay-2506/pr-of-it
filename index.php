c8029b8c3e042b21fa52301ce55145ad1eb3f21f
Nikolay-2506 <40524261+Nikolay-2506@users.noreply.github.com>
2019-03-19 16:48:49 +0700

    Corrections
    Corrections

--- index.php
+++ index.php
@@ -7,12 +7,6 @@ require __DIR__ . '/autoload.php';
 
 $article = new Article;
 
-//$sql = 'SELECT * FROM news WHERE id=:id';
-//$data = $db->query($sql, [':id' => 2 ]);
-
-//$data = Article::findAll();
 $data = Article::findLastThree();
 
 include __DIR__ . '/Template/tableNews.php';
-
-//echo '<pre>'.print_r($data, true).'</pre>';
